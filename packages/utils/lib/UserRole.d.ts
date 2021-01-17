378900e444d3c5944202c6aeb30f780ba044f736
xTCry [Vladislav Kh] <wolt6@mail.ru>
2021-01-17 22:40:13 +0300

    chore(utils): ignore lib folder

--- packages/utils/lib/UserRole.d.ts
+++
@@ -1,8 +0,0 @@
-export declare enum UserRole {
-    NONE = 1,
-    USER = 2,
-    STUDENT = 3,
-    TEACHER = 4,
-    DEKAN = 5,
-    ADMIN = 6
-}
--- packages/utils/lib/logger.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="signale" />
-import ololog from 'ololog';
-export declare const olog: ololog;
-export declare const slog: import("signale").Signale<import("signale").DefaultMethods>;
