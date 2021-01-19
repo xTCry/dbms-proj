732ea7962a8ec35020536a4f966979f59093e040
xTCry [Vladislav Kh] <wolt6@mail.ru>
2021-01-19 13:28:16 +0300

    Update destroy.ts

--- packages/server/src/crud/destroy.ts
+++ packages/server/src/crud/destroy.ts
@@ -1,3 +1,5 @@
+import { olog } from '@dbms-proj/utils';
+import Boom from '@hapi/boom';
 import { RequestHandler } from 'express';
 import { Controller } from '../controllers/controller';
 import { IUserJSON } from '../controllers/user.controller';
@@ -6,8 +8,20 @@ import { RequestWith } from '../tools/auth';
 export const destroy = (ctrl: typeof Controller): RequestHandler => async (req, res, next) => {
     try {
         const { user } = req as RequestWith<{ user?: IUserJSON }>;
-        await ctrl.doDestroy(req.params.id, user?.role_id);
-        res.jsongo({ id: req.params.id });
+
+        if (req.params.id) {
+            const { id } = req.params;
+            await ctrl.doDestroy(id, user?.role_id);
+            res.jsongo({ id });
+        } else {
+            const query = req.query as any;
+            olog.debug('query', query);
+
+            const ids = query.filter.id;
+            olog.debug('ids', ids);
+
+            return next(Boom.notImplemented('Delete not worked'));
+        }
     } catch (error) {
         next(error);
     }
