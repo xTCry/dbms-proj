95325925f6dbc02085ec87d96d92352bc0d128e8
xTCry [Vladislav Kh] <wolt6@mail.ru>
2021-01-21 03:22:29 +0300

    chore(client): added app footer

--- packages/client/src/containers/App.tsx
+++ packages/client/src/containers/App.tsx
@@ -13,20 +13,36 @@ import { LoginWithTheme } from '../components/Login';
 class App extends Component {
     render() {
         return (
-            <Admin
-                title="YControl Panel"
-                history={history}
-                {...Providers}
-                customRoutes={Routes}
-                layout={Layout}
-                loginPage={LoginWithTheme}
-                catchAll={NotFound}
-                dashboard={Dashboard}
-            >
-                {permissions => Object.values(Resources).map((res, i) => (
-                    <Resource {...res(permissions)} key={i} />
-                ))}
-            </Admin>
+            <>
+                <Admin
+                    title="YControl Panel"
+                    history={history}
+                    {...Providers}
+                    customRoutes={Routes}
+                    layout={Layout}
+                    loginPage={LoginWithTheme}
+                    catchAll={NotFound}
+                    dashboard={Dashboard}
+                >
+                    {(permissions) =>
+                        Object.values(Resources).map((res, i) => <Resource {...res(permissions)} key={i} />)
+                    }
+                </Admin>
+                <div
+                    style={{
+                        position: 'fixed',
+                        right: 0,
+                        bottom: 0,
+                        left: 0,
+                        zIndex: 100,
+                        padding: 6,
+                        backgroundColor: '#efefef',
+                        textAlign: 'center',
+                    }}
+                >
+                    <strong>v1.0.0</strong>
+                </div>
+            </>
         );
     }
 }
